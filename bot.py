c4e1b772a57f1d4ccedd3a41226ada302b364d7e
Laikaiyong <vandycklai@gmail.com>
2021-08-05 23:52:46 +0800

    Set status change

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ cogs = [main]
 intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix=main.Main.get_prefix, intents=intents, help_command=None,
-                      activity=discord.Activity(type=discord.ActivityType.competing, name="Vandyck#7726 兄ちゃん戦争"))
+                      activity=discord.Activity(type=discord.ActivityType.watching, name="Data Science"))
 
 for cog in cogs:
     cog.setup(client)
