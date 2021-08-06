3f3598cbb364bf0593270cea510bb8a5bd57fa62
Laikaiyong <vandycklai@gmail.com>
2021-08-06 14:38:57 +0800

    Reactrole file and json db setup

---
+++ reactrole.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+
+
+class React(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+
+def setup(client):
+    client.add_cog(React(client))
