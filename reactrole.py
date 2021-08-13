0934eed49ecf368b8447e8752a0f0daf41e0df86
Laikaiyong <vandycklai@gmail.com>
2021-08-13 20:18:26 +0800

    Draft code section

--- reactrole.py
+++ reactrole.py
@@ -1,11 +1,14 @@
 import discord
 from discord.ext import commands
+import json
 
 
 class React(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+
+    # React role
 
 
 def setup(client):
