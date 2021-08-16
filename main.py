e18770cdf81ff877543b1335321abad7f52c7e24
Laikaiyong <vandycklai@gmail.com>
2021-08-16 20:35:51 +0800

    Add kick and ban features

--- main.py
+++ main.py
@@ -56,8 +56,33 @@ class Main(commands.Cog):
     # Welcome
 
     # Kick
+    @commands.command()
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        if ctx.author.guild_permissions.kick_members:
+            await member.kick(reason=reason)
+            embed = discord.Embed(
+                title="Kick",
+                description=f"**{member}** was kicked.",
+                color=discord.Color.red()
+            )
+            embed.set_thumbnail(url=member.avatar_url)
+            embed.add_field(name="Reason", value=f"{reason}")
+            await ctx.send(embed=embed)
 
     # Ban
+
+    @commands.command()
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+        if ctx.author.guild_permissions.ban_members:
+            await member.ban(reason=reason)
+            embed = discord.Embed(
+                title="Ban",
+                description=f"**{member}** was banned.",
+                color=discord.Color.red()
+            )
+            embed.set_thumbnail(url=member.avatar_url)
+            embed.add_field(name="Reason", value=f"{reason}")
+            await ctx.send(embed=embed)
 
 
 def setup(client):
